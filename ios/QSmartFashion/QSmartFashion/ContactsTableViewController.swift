270325a4dea62a5d4b3ac16272361ee1e0acaf2b
tkatzenbaer <me@katzenbaer.net>
2015-08-02 19:06:35 -0700

    Add Delete Contact feature

--- ios/QSmartFashion/QSmartFashion/ContactsTableViewController.swift
+++ ios/QSmartFashion/QSmartFashion/ContactsTableViewController.swift
@@ -12,6 +12,7 @@ import Parse
 
 class ContactsTableViewController: UITableViewController, CNContactPickerDelegate {
 
+    @IBOutlet var addBarButtonItem: UIBarButtonItem!
     var contacts = [SFEmergencyContact]()
     
     override func viewDidLoad() {
@@ -21,7 +22,7 @@ class ContactsTableViewController: UITableViewController, CNContactPickerDelegat
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        self.navigationItem.rightBarButtonItems = [addBarButtonItem, self.editButtonItem()]
     }
     
     override func viewDidAppear(animated: Bool) {
@@ -57,10 +58,6 @@ class ContactsTableViewController: UITableViewController, CNContactPickerDelegat
         print("returning \(self.contacts.count) rows")
         return self.contacts.count
     }
-    
-//    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-//        return 55
-//    }
 
     @IBAction func showPicker(sender: UIBarButtonItem) {
         let picker = CNContactPickerViewController()
@@ -132,25 +129,40 @@ class ContactsTableViewController: UITableViewController, CNContactPickerDelegat
         return cell
     }
 
-    /*
-    // Override to support conditional editing of the table view.
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
 
-    /*
-    // Override to support editing the table view.
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
-            // Delete the row from the data source
+            let contact = contacts[indexPath.row]
+            contacts.removeAtIndex(indexPath.row)
+            contact.deleteInBackgroundWithBlock {
+                (succeeded: Bool, error: NSError?) -> Void in
+                if succeeded {
+                    print("deleted '\(contact.name)' from my contacts!")
+                } else {
+                    print("unable to delete contact")
+                }
+            }
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
         } else if editingStyle == .Insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+    
+    override func setEditing(editing: Bool, animated: Bool) {
+        super.setEditing(editing, animated: animated)
+        
+        let barButtonItems: [UIBarButtonItem]
+        if editing {
+            barButtonItems = [self.editButtonItem()]
+        } else {
+            barButtonItems = [addBarButtonItem, self.editButtonItem()]
+        }
+        self.navigationItem.setRightBarButtonItems(barButtonItems, animated: true)
+    }
 
     /*
     // Override to support rearranging the table view.
