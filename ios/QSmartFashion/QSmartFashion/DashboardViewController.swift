b9115a2f2b05dd26161e2c7aff66e35115b7cff0
tkatzenbaer <me@katzenbaer.net>
2015-08-04 16:09:08 -0700

    Add Dashboard VC

---
+++ ios/QSmartFashion/QSmartFashion/DashboardViewController.swift
@@ -0,0 +1,54 @@
+//
+//  DashboardViewController.swift
+//  QSmartFashion
+//
+//  Created by Terrence Katzenbaer on 8/4/15.
+//  Copyright © 2015 Qualcomm Incorporated. All rights reserved.
+//
+
+import UIKit
+
+class DashboardViewController: UIViewController {
+    
+    @IBOutlet var heartHistoryView: UIWebView
+    @IBOutlet var temperatureHistoryView: UIWebView
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let baseUrl = NSBundle.mainBundle().bundleURL
+        let heartHistoryHtmlPath = NSBundle.mainBundle().pathForResource("cv", ofType: "html")
+        let temperatureHistoryHtmlPath = heartHistoryHtmlPath
+        
+        do {
+            let heartHistoryHtml = try NSString(contentsOfFile: heartHistoryHtmlPath, encoding: NSUTF8StringEncoding)
+            heartHistoryView.loadHTMLString(heartHistoryHtml, baseURL: baseUrl)
+        } catch {
+            print("unable to load heart history html")
+        }
+        
+        do {
+            let temperatureHistoryHtml = try NSString(contentsOfFile: temperatureHistoryHtmlPath, encoding: NSUTF8StringEncoding)
+            temperatureHistoryView.loadHTMLString(temperatureHistoryHtml, baseURL: baseUrl)
+        } catch {
+            print("unable to load heart history html")
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
